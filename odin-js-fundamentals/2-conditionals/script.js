90121a10a91ac0df33cf32b2682aecf14a525472
DerekNguyenn <dereknguyenbusiness@gmail.com>
2023-02-21 13:24:02 -0800

    Initial commit

---
+++ odin-js-fundamentals/2-conditionals/script.js
@@ -0,0 +1,10 @@
+// Write the code which asks for a login with prompt
+// Based on https://javascript.info/task/check-login
+
+// If the visitor enters "Admin", then prompt for a password. 
+// If the input is an empty line or Esc – show “Canceled”.
+// If it’s another string – then show “I don’t know you”.
+
+
+let username = prompt("Please enter your name");
+let password = "TheMaster";
