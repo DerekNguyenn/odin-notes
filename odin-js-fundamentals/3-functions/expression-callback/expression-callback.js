ab128907e9f90a69224c55335d6e2b5131e7dfd3
DerekNguyenn <dereknguyenbusiness@gmail.com>
2023-02-26 14:02:11 -0800

    Replace arguments with function expression

--- odin-js-fundamentals/3-functions/expression-callback/expression-callback.js
+++ odin-js-fundamentals/3-functions/expression-callback/expression-callback.js
@@ -1,2 +1,21 @@
 // Exercise based on https://javascript.info/function-expressions#callback-functions
 // Given the code in reference.js, condense it down by rewriting it using function expressions
+
+function ask(question, yes, no) {
+	if (confirm(question)) yes()
+	else no();
+}
+
+function showOk() {
+	alert( "You agreed." );
+}  
+
+function showCancel() {
+	alert( "You canceled the execution." );
+}
+
+ask(
+	"Do you agree?",
+  	function() {alert("You agreed.")},
+  	function() {alert("You canceled the execution.")}
+)
