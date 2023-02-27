f6e5f68eb824df862912ab743f12154aeae1f941
DerekNguyenn <dereknguyenbusiness@gmail.com>
2023-02-27 01:16:01 -0800

    Update exercise instructions

--- odin-js-fundamentals/3-functions/expression-rewrite/rewrite-welcome.js
+++ odin-js-fundamentals/3-functions/expression-rewrite/rewrite-welcome.js
@@ -3,3 +3,7 @@
 // function expression and function declaration.
 // Given the following code in bugged-welcome.js, rewrite the code with function expression
 // so that the call to welcome() does not return an error.
+// welcome should contain the source code of either alert("Hello!") or alert("Greetings"). 
+// To check, use alert or console.log without parentheses on welcome.
+
+let age = prompt("What is your age?", 18);
