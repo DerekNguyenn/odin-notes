01c5d1d1b252f206f7723acf8a0b01d73c95359b
DerekNguyenn <dereknguyenbusiness@gmail.com>
2023-02-27 01:07:48 -0800

    Initial commit

---
+++ odin-js-fundamentals/3-functions/expression-rewrite/rewrite-welcome.js
@@ -0,0 +1,5 @@
+// Exercise based on https://javascript.info/function-expressions#function-expression-vs-function-declaration
+// This exercise highlights the importance of function scope when using
+// function expression and function declaration.
+// Given the following code in bugged-welcome.js, rewrite the code with function expression
+// so that the call to welcome() does not return an error.
