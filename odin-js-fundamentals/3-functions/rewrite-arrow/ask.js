1e869f2664a41c494de904fb8a4e9d21fea959b0
DerekNguyenn <dereknguyenbusiness@gmail.com>
2023-02-27 12:07:21 -0800

    Fix tabbing

--- odin-js-fundamentals/3-functions/rewrite-arrow/ask.js
+++ odin-js-fundamentals/3-functions/rewrite-arrow/ask.js
@@ -7,10 +7,10 @@ it utilizes arrow functions.
 function ask(question, yes, no) {
     if (confirm(question)) yes();
     else no();
-  }
-  
-  ask(
+}
+
+ask(
     "Do you agree?",
     function() { alert("You agreed."); },
     function() { alert("You canceled the execution."); }
-  );
+);
