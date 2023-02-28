4b482d9f9604980bafaf771f5c74ac8214edafb8
DerekNguyenn <dereknguyenbusiness@gmail.com>
2023-02-27 16:34:20 -0800

    Create user input validation

--- odin-js-fundamentals/3-functions/fizzbuzz.js
+++ odin-js-fundamentals/3-functions/fizzbuzz.js
@@ -7,6 +7,16 @@ For numbers which are multiples of both three and five print `FizzBuzz`.
 
 let number = prompt("Enter a number");
 
+function validateInput(n) {
+	return (n < 0) ? "Please enter a non-negative integer"
+  : (n === "") ? "No number entered"
+  : (!n) ? "Program has been canceled"
+  : (isNaN(Number(n))) ? "Invalid number."
+  : `Your number is ${number}`;
+}
+
+console.log(validateInput(number));
+
 for (let i = 1; i <= number; i++) {
     console.log(i);
   }
