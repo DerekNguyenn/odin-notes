94aea8c02c50058acf7a663061cfb7d241e8492a
DerekNguyenn <dereknguyenbusiness@gmail.com>
2023-02-27 16:33:58 -0800

    Prompt for input and print increment

--- odin-js-fundamentals/3-functions/fizzbuzz.js
+++ odin-js-fundamentals/3-functions/fizzbuzz.js
@@ -4,3 +4,9 @@ the number the user entered. However, for multiples of three print `Fizz` 
 instead of the number and for the multiples of five print `Buzz`. 
 For numbers which are multiples of both three and five print `FizzBuzz`.
 */
+
+let number = prompt("Enter a number");
+
+for (let i = 1; i <= number; i++) {
+    console.log(i);
+  }
