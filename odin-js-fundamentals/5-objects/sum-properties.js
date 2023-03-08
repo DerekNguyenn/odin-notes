566813b8ce4407d8a64a685c98fc88d0aebf7de2
DerekNguyenn <dereknguyenbusiness@gmail.com>
2023-03-08 13:24:30 -0800

    Initial commit

---
+++ odin-js-fundamentals/5-objects/sum-properties.js
@@ -0,0 +1,12 @@
+/*
+Exercise from https://javascript.info/task/sum-object
+Given the following object and its properties, write code
+to sum all salaries and store in the variable sum.
+If salaries is empty, then the result must be 0.
+*/
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+};
